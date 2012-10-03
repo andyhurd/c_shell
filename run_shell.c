2766ae0032916010c375c25636e6ef29b2396c4c
Andy Hurd <ahurd@adc4gis.com>
2012-10-03 12:05:57 -0500

    Restores append redirection and splits commands

--- run_shell.c
+++ run_shell.c
@@ -14,10 +14,11 @@
 #include <signal.h>
 #include <string.h>
 
-#define DEBUG 1
+#define DEBUG 0
 
 extern char **my_getline();
-int append_output(char **args, char **output_filename);
+int *splitCommands(char ****commands, char **args);
+void executeCommand(char **args);
 void tilde(char **args);
 
 /*
@@ -37,12 +38,8 @@ void sig_handler(int signal) {
 main() {
   int i;
   char **args; 
-  int result;
-  int block;
-  int output;
-  int input;
-  char *output_filename;
-  char *input_filename;
+  int *nCommands;
+  char ***commands;
 
   // Set up the signal handler
   sigset(SIGCHLD, sig_handler);
@@ -54,64 +51,92 @@ main() {
     printf("->");
     args = my_getline();
 
-    // No input, continue
-    if(args[0] == NULL)
-      continue;
+    // Check for command delimiters
+    nCommands = splitCommands(&commands, args);
 
-    // Check for internal shell commands, such as exit
-    if(internal_command(args))
-      continue;
-
-    // Check for tilde (~)
-    tilde(args);
-
-    // Check for an ampersand
-    block = (ampersand(args) == 0);
-
-    // Check for redirected input
-    input = redirect_input(args, &input_filename);
-
-    switch(input) {
-    case -1:
-      printf("Syntax error!\n");
-      continue;
-      break;
-    case 0:
-      break;
-    case 1:
-      if (DEBUG)
-        printf("Redirecting input from: %s\n", input_filename);
-      break;
+    int j;
+    for (i = 0; i < *nCommands; i++) {
+      if (DEBUG) {
+        printf("\nTESTING: ");
+        for (j = 0; commands[i][j] != NULL; j++) {
+          printf("%s ", commands[i][j]);
+        }
+        printf("\n");
+      }
+      executeCommand(commands[i]);
     }
-
-    // Check for redirected output
-    output = redirect_output(args, &output_filename);
-
-    switch(output) {
-    case -1:
-        printf("Syntax error!\n");
-      continue;
-      break;
-    case 0:
-      break;
-    case 1:
-      if (DEBUG)
-        printf("Redirecting (Overriding) output to: %s\n", output_filename);
-      break;
-    case 2:
-      if (DEBUG)
-        printf("Redirecting (Appending) output to: %s\n", output_filename);
-      break;
-    }
-
-    // Do the command
-    do_command(args, block, 
-	       input, input_filename, 
-	       output, output_filename);
-
-    for (i = 0; args[i] != NULL; i++)
-      free(args[i]);
   }
+}
+
+void executeCommand(char **args) {
+  int i;
+  int result;
+  int block;
+  int output;
+  int input;
+  char *output_filename;
+  char *input_filename;
+
+  if (DEBUG)
+    printf("in executeCmd");
+
+  // No input, continue
+  if(args[0] == NULL)
+    return;
+
+  // Check for internal shell commands, such as exit
+  if(internal_command(args))
+    return;
+
+  // Check for tilde (~)
+  tilde(args);
+
+  // Check for an ampersand
+  block = (ampersand(args) == 0);
+
+  // Check for redirected input
+  input = redirect_input(args, &input_filename);
+
+  switch(input) {
+  case -1:
+    printf("Syntax error!\n");
+    return;
+    break;
+  case 0:
+    break;
+  case 1:
+    if (DEBUG)
+      printf("Redirecting input from: %s\n", input_filename);
+    break;
+  }
+
+  // Check for redirected output
+  output = redirect_output(args, &output_filename);
+
+  switch(output) {
+  case -1:
+      printf("Syntax error!\n");
+    return;
+    break;
+  case 0:
+    break;
+  case 1:
+    if (DEBUG)
+      printf("Redirecting (Overriding) output to: %s\n", output_filename);
+    break;
+  case 2:
+    if (DEBUG)
+      printf("Redirecting (Appending) output to: %s\n", output_filename);
+    break;
+  }
+
+  // Do the command
+  do_command(args, block, 
+       input, input_filename, 
+       output, output_filename);
+
+  for (i = 0; args[i] != NULL; i++)
+    free(args[i]);
 }
 
 /*
@@ -268,20 +293,22 @@ int redirect_output(char **args, char **output_filename) {
 
     // Look for the >
     if(args[i][0] == '>') {
-      if(args[i][1] == '>')
+      if(args[i+1] != NULL && args[i+1][0] == '>') {
+        free(args[i+1]);
         redirectType = 2;
+      }
       free(args[i]);
 
       // Get the filename 
-      if(args[i+1] != NULL) {
-	      *output_filename = args[i+1];
+      if(args[i+redirectType] != NULL) {
+	      *output_filename = args[i+redirectType];
       } else {
 	      return -1;
       }
 
-      // Adjust the rest of the arguments in the array
+      // Adjust the rest of the array
       for(j = i; args[j-1] != NULL; j++) {
-	      args[j] = args[j+2];
+	      args[j] = args[j+1+redirectType];
       }
 
       return redirectType;
@@ -289,5 +316,105 @@ int redirect_output(char **args, char **output_filename) {
   }
 
   return 0;
+}
+
+int *splitCommands(char ****commands, char **args) {
+  int cmdStart = 0;
+  int *nCommands = (int *)malloc(sizeof(int));
+  *nCommands = 0;
+  int numArgs = 0;
+  int i;
+  char *buffer;
+
+  // find number of commands total
+  for (i = 0; args[i] != NULL; i++) {
+    if (args[i][0] == ';') {
+      (*nCommands)++;
+    }
+  }
+
+  // find number of arguments total
+  for (i = 0; args[i] != NULL; i++) {
+    numArgs++;
+  }
+
+  // allocate memory for array of commands
+  *commands = (char ***)malloc(sizeof(char **) * (*nCommands + 1));
+
+  *nCommands = 0;
+
+  // build commands that are terminated with semicolon
+  for (i = 0; args[i] != NULL; i++) {
+    if (args[i][0] == ';') {
+      int argIndex;
+
+      // allocate memory for new command
+      (*commands)[*nCommands] = (char **)malloc(sizeof(char *) * (i - cmdStart + 1));
+
+      int j;
+      for(j = cmdStart; j < i; j++) {
+        // set argIndex
+        argIndex = j - cmdStart;
+
+        // copy argument to a buffer char *
+        int length = strlen(args[j]);
+        buffer = (char *)malloc(sizeof(char) * (length + 1)); 
+        strcpy(buffer, args[j]);
+        buffer[length] = '\0';
+
+        // point the next arg of current command to buffer
+        (*commands)[*nCommands][argIndex] = buffer;
+
+        // free memory 
+        free(args[j]);
+      }
+
+      // free memory
+      free(args[i]);
+
+      // null terminate this command's array of args
+      argIndex++;
+      (*commands)[*nCommands][argIndex] = NULL;
+
+      cmdStart = i + 1;
+      (*nCommands)++;
+    }
+  }
+
+  // build commands that are terminated at EOF
+  if (args[cmdStart] != NULL) {
+    int argIndex;
+
+    // allocate memory for new command
+    (*commands)[*nCommands] = (char **)malloc(sizeof(char *) * (numArgs - cmdStart + 1));
+
+    for (i = cmdStart; args[i] != NULL; i++) {
+      // set argIndex
+      argIndex = i - cmdStart;
+
+      // copy argument to a buffer char *
+      int length = strlen(args[i]);
+      buffer = (char *)malloc(sizeof(char) * (length + 1)); 
+      strcpy(buffer, args[i]);
+      buffer[length] = '\0';
+
+      // point the next arg of current command to buffer
+      (*commands)[*nCommands][argIndex] = buffer;
+
+      // free memory 
+      free(args[i]);
+    }
+
+    // null terminate this command's array of args
+    argIndex = i - cmdStart;
+    (*commands)[*nCommands][argIndex] = NULL;
+
+    (*nCommands)++;
+  }
+
+  // null terminate array of commands
+  (*commands)[*nCommands] = NULL;
+
+  return nCommands;
 }
 
